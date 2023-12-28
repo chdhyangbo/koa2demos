1a6cdda2fd3366c5c8c13dfef409c37e1e66a33a
SteveYe-YJB <790021919@qq.com>
2023-12-28 22:43:09 +0800

    fixbug:新增接口

--- routers/smsCode.js
+++ routers/smsCode.js
@@ -47,4 +47,38 @@ router.post('/smsCode/query', async (ctx, next) => {
   ctx.body = body
 })
 
+router.post('/smsCode/voodooQuery', async (ctx, next) => {
+  const { searchNumber, ip = '', city = '' } = ctx.request.body;
+
+  let codeItem = await DB.find('voodooSmsCode', {
+    value: searchNumber
+  })
+  console.log(codeItem)
+  codeItem = codeItem[0]
+  let body = {}
+  if (codeItem && codeItem._id) {
+    await DB.update('voodooSmsCode', { "_id": DB.getObjectId(codeItem._id) }, {
+      ...codeItem,
+      queryTime: [
+        ...codeItem.queryTime,
+        {
+          time: moment(new Date()).format('YYYY-MM-DD HH:mm:ss'),
+          ip,
+          city,
+        }
+      ]
+    });
+    body = {
+      message: '校验成功',
+      ...codeItem,
+      cc: 0
+    };
+  } else {
+    body = {
+      message: '查询失败',
+      cc: 1
+    }
+  }
+  ctx.body = body
+})
 module.exports = router;
