df635e57a2ec850ac0b46a90c20dca876997fe38
liguantu <8154799+liguantu@user.noreply.gitee.com>
2023-12-30 11:07:46 +0800

    LGT12301107

--- routers/smsCode.js
+++ routers/smsCode.js
@@ -11,6 +11,7 @@ let router = new Router({
 });
 const DB = require('../db/index.js');
 const moment = require('moment');
+const { async } = require('node-stream-zip');
 
 router.post('/smsCode/query', async (ctx, next) => {
   const { searchNumber, ip = '', city = '' } = ctx.request.body;
@@ -81,4 +82,41 @@ router.post('/smsCode/voodooQuery', async (ctx, next) => {
   }
   ctx.body = body
 })
+
+
+router.post('/smsCode/elfp', async (ctx, next) => {
+  const { searchNumber, ip = '', city = '' } = ctx.request.body;
+
+  let codeItem = await DB.find('elfpSmsCode', {
+    value: searchNumber
+  })
+  console.log(codeItem)
+  codeItem = codeItem[0]
+  let body = {}
+  if (codeItem && codeItem._id) {
+    await DB.update('elfpSmsCode', { "_id": DB.getObjectId(codeItem._id) }, {
+      ...codeItem,
+      queryTime: [
+        ...codeItem.queryTime,
+        {
+          time: moment(new Date()).format('YYYY-MM-DD HH:mm:ss'),
+          ip,
+          city,
+        }
+      ]
+    });
+    body = {
+      message: '校验成功',
+      ...codeItem,
+      cc: 0
+    };
+  } else {
+    body = {
+      message: '查询失败',
+      cc: 1
+    }
+  }
+  ctx.body = body
+})
+
 module.exports = router;
